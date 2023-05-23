251e40d94c201f06a909dfac823daaa1ffc93f35
Clément <clement@numericite.eu>
2023-05-23 10:48:26 +0200

    feat: improve fake data script

--- faker/main.py
+++ faker/main.py
@@ -3,22 +3,43 @@ import random
 from faker import Faker
 from datetime import date
 
-fake = Faker()
+fake = Faker('fr_FR')
+
+department_dict = {
+    '75': ['Paris', 'Paris 2', 'Paris 3'],
+    '77': ['Meaux', 'Chelles', 'Melun'],
+    '78': ['Versailles', 'Yvelines', 'Rambouillet'],
+    '91': ['Evry', 'Massy', 'Palaiseau'],
+    '92': ['Nanterre', 'Boulogne-Billancourt', 'Colombes'],
+    '93': ['Saint-Denis', 'Montreuil', 'Aubervilliers'],
+    '94': ['Créteil', 'Vincennes', 'Ivry-sur-Seine'],
+    '95': ['Cergy', 'Argenteuil', 'Sarcelles'],
+}
+
+category_1 = ['suicide', 'avc', 'cancer', 'tuberculose']
+category_2 = ['vih', 'tuberculose', 'diabete']
 
 with open('sample_data.csv', mode='w', newline='') as csv_file:
-    fieldnames = ['categories_level_1', 'categories_level_2', 'age', 'sex', 'death_location', 'home_location', 'coordinates', 'date']
+    fieldnames = ['categories_level_1', 'categories_level_2', 'age', 'sex', 'death_location', 'home_location', 'department', 'coordinates', 'date']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
     writer.writeheader()
 
     for i in range(100000):
         row = {}
-        row['categories_level_1'] = random.choice(['suicide', 'avc', 'cancer', 'tuberculose'])
-        row['categories_level_2'] = random.choice(['vih', 'tuberculose', 'diabete'])
+        
+        if random.random() < 0.2:  # 20% chance to have multiple categories
+            row['categories_level_1'] = ", ".join(random.sample(category_1, random.randint(2, len(category_1))))
+            row['categories_level_2'] = ", ".join(random.sample(category_2, random.randint(2, len(category_2))))
+        else:
+            row['categories_level_1'] = random.choice(category_1)
+            row['categories_level_2'] = random.choice(category_2)
+
         row['age'] = fake.random_int(min=1, max=100)
-        row['sex'] = random.choice(['homme', 'femme'])
+        row['sex'] = random.choice(['homme', 'femme', 'indéterminé'])
         row['death_location'] = random.choice(['domicile', 'hopital', 'autre'])
-        row['home_location'] = fake.city()
+        row['department'] = random.choice(list(department_dict.keys()))
+        row['home_location'] = random.choice(department_dict[row['department']])
         row['coordinates'] = f"{fake.latitude()}, {fake.longitude()}"
         row['date'] = fake.date_between(start_date=date(2021, 1, 1), end_date=date(2023, 12, 31)).isoformat()
 
