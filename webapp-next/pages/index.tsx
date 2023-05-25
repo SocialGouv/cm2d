913a45d65633c9c4b304107eb1c79a0d1ae1ede7
H4CK3R5-Dmaster <h4ck3r5pc@gmail.com>
2023-05-25 10:57:37 +0200

    fix: options landing elements

--- webapp-next/pages/index.tsx
+++ webapp-next/pages/index.tsx
@@ -1,19 +1,60 @@
-import { BlockArticles } from '@/components/landinglayout/BlockArticles';
-import ColumnWithImage from '@/components/landinglayout/BlockColumnwithimage';
-import BlockGaleryimages from '@/components/landinglayout/BlockGaleryImages';
-import BlockImageGrids from '@/components/landinglayout/BlockImageGrids';
-import ContainerBlock from '@/components/landinglayout/ContainerBlock';
-import { Footer } from '@/components/landinglayout/Footer';
-import NavbarLanding from '@/components/landinglayout/NavbarLanding';
+import { BlockArticles } from "@/components/landinglayout/BlockArticles";
+import ColumnWithImage from "@/components/landinglayout/BlockColumnwithimage";
+import BlockGaleryimages from "@/components/landinglayout/BlockGaleryImages";
+import { BlockImageGrids } from "@/components/landinglayout/BlockImageGrids";
+import ContainerBlock from "@/components/landinglayout/ContainerBlock";
+import { Footer } from "@/components/landinglayout/Footer";
+import NavbarLanding from "@/components/landinglayout/NavbarLanding";
 
 export default function Landing() {
   return (
     <ContainerBlock>
       <NavbarLanding />
       <ColumnWithImage />
-      <BlockGaleryimages />
-      <BlockImageGrids />
-      <BlockArticles />
+      <BlockGaleryimages
+        Imagescontent={[
+          { imagelink: "/placeholder-img.svg" },
+          { imagelink: "/placeholder-img.svg" },
+          { imagelink: "/placeholder-img.svg" },
+        ]}
+      />
+      <BlockImageGrids
+        contentimage={{ image: "/Placeholder-image.svg" }}
+        contentsgrids={[
+          {
+            title: "Lorem Ipsum",
+            description:
+              "Lörem ipsum bominar mivit om tynade. Hybridkrig bodyssa biosedore preren. Homode krorad hänåns. Nisade nektig vans. Tyde plamoplask osade intrarad dirysk res. Vilina tetrade har automoskap jag homoskade. Pibel miment. ",
+          },
+          {
+            title: "Lorem Ipsum",
+            description:
+              "Lörem ipsum bominar mivit om tynade. Hybridkrig bodyssa biosedore preren. Homode krorad hänåns. Nisade nektig vans. Tyde plamoplask osade intrarad dirysk res. Vilina tetrade har automoskap jag homoskade. Pibel miment. ",
+          },
+        ]}
+      />
+      <BlockArticles
+        article={[
+          {
+            title: "SubTitle Lorem ipsum",
+            description:
+              "Lörem ipsum bominar mivit om tynade. Hybridkrig bodyssa biosedore preren. Homode krorad hänåns. Nisade nektig vans. Tyde plamoplask osade intrarad dirysk res. Vilina tetrade har.",
+            image: "/PlaceholderImageArticle.svg",
+          },
+          {
+            title: "SubTitle Lorem ipsum",
+            description:
+              "Lörem ipsum bominar mivit om tynade. Hybridkrig bodyssa biosedore preren. Homode krorad hänåns. Nisade nektig vans. Tyde plamoplask osade intrarad dirysk res. Vilina tetrade har.",
+            image: "/PlaceholderImageArticle.svg",
+          },
+          {
+            title: "SubTitle Lorem ipsum",
+            description:
+              "Lörem ipsum bominar mivit om tynade. Hybridkrig bodyssa biosedore preren. Homode krorad hänåns. Nisade nektig vans. Tyde plamoplask osade intrarad dirysk res. Vilina tetrade har.",
+            image: "/PlaceholderImageArticle.svg",
+          },
+        ]}
+      />
       <Footer />
     </ContainerBlock>
   );
