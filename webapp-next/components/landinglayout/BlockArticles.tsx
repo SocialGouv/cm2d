229646c9cb9ef6f285bf372924fd13360c19eb4e
H4CK3R5-Dmaster <h4ck3r5pc@gmail.com>
2023-05-25 10:56:44 +0200

    fix: options landing element

--- webapp-next/components/landinglayout/BlockArticles.tsx
+++ webapp-next/components/landinglayout/BlockArticles.tsx
@@ -14,7 +14,16 @@ import {
 } from "@chakra-ui/react";
 import NextLink from "next/link";
 
-export const BlockArticles = () => {
+type Articles = {
+  title: string;
+  description: string;
+  image: string;
+};
+interface Article {
+  article: Articles[];
+}
+
+export const BlockArticles: React.FC<Article> = ({ article }) => {
   return (
     <Flex>
       <Box mt={[10, 14]} px={[5, 16, 36]}>
@@ -27,123 +36,45 @@ export const BlockArticles = () => {
           Lorem ipsum
         </Text>
 
-        <Box w={"full"}>
-          <Wrap my={[5, 10]} mb={10}>
-            <WrapItem flex={1}>
-              <Box>
-                <Text
-                  fontSize={["16px", "18px", "24px"]}
-                  fontWeight={500}
-                  color={"neutral.600"}
-                  mb={[5, 5, 5]}
-                >
-                  SubTitle Lorem ipsum
-                </Text>
-                <Text color={"neutral.500"} fontWeight={400} mb={[2, 5, 5]}>
-                  Lörem ipsum bominar mivit om tynade. Hybridkrig bodyssa
-                  biosedore preren. Homode krorad hänåns. Nisade nektig vans.
-                  Tyde plamoplask osade intrarad dirysk res. Vilina tetrade har.
-                </Text>
-                <NextLink href={"/landingpage"}>
-                  <Button
-                    variant="outline"
-                    borderRadius={5}
-                    borderColor={"neutral.500"}
-                    color={"neutral.500"}
-                    _hover={{}}
-                    height="12"
-                    width={"40"}
+        {article.map((art, index) => (
+          <Box w={"full"} key={index}>
+            <Wrap my={[5, 10]} mb={10}>
+              <WrapItem flex={1}>
+                <Box>
+                  <Text
+                    fontSize={["16px", "18px", "24px"]}
+                    fontWeight={500}
+                    color={"neutral.600"}
+                    mb={[5, 5, 5]}
                   >
-                    Button
-                  </Button>
-                </NextLink>
-              </Box>
-            </WrapItem>
-            <WrapItem>
-              <Box>
-                <Image src="/PlaceholderImageArticle.svg" />
-              </Box>
-            </WrapItem>
-          </Wrap>
-        </Box>
-        <Box w={"full"}>
-          <Wrap my={[5, 10]} mb={10}>
-            <WrapItem flex={1}>
-              <Box>
-                <Text
-                  fontSize={["16px", "18px", "24px"]}
-                  fontWeight={500}
-                  color={"neutral.600"}
-                  mb={[2, 5, 5]}
-                >
-                  SubTitle Lorem ipsum
-                </Text>
-                <Text color={"neutral.500"} fontWeight={400} mb={[2, 5, 5]}>
-                  Lörem ipsum bominar mivit om tynade. Hybridkrig bodyssa
-                  biosedore preren. Homode krorad hänåns. Nisade nektig vans.
-                  Tyde plamoplask osade intrarad dirysk res. Vilina tetrade har.
-                </Text>
-                <NextLink href={"/landingpage"}>
-                  <Button
-                    variant="outline"
-                    borderRadius={5}
-                    borderColor={"neutral.500"}
-                    color={"neutral.500"}
-                    _hover={{}}
-                    height="12"
-                    width={"40"}
-                  >
-                    Button
-                  </Button>
-                </NextLink>
-              </Box>
-            </WrapItem>
-            <WrapItem>
-              <Box>
-                <Image src="/PlaceholderImageArticle.svg" />
-              </Box>
-            </WrapItem>
-          </Wrap>
-        </Box>
-        <Box w={"full"}>
-          <Wrap my={[5, 10]} mb={10}>
-            <WrapItem flex={1}>
-              <Box>
-                <Text
-                  fontSize={["16px", "18px", "24px"]}
-                  fontWeight={500}
-                  color={"neutral.600"}
-                  mb={[2, 5, 5]}
-                >
-                  SubTitle Lorem ipsum
-                </Text>
-                <Text color={"neutral.500"} fontWeight={400} mb={[2, 5, 5]}>
-                  Lörem ipsum bominar mivit om tynade. Hybridkrig bodyssa
-                  biosedore preren. Homode krorad hänåns. Nisade nektig vans.
-                  Tyde plamoplask osade intrarad dirysk res. Vilina tetrade har.
-                </Text>
-                <NextLink href={"/landingpage"}>
-                  <Button
-                    variant="outline"
-                    borderRadius={5}
-                    borderColor={"neutral.500"}
-                    color={"neutral.500"}
-                    _hover={{}}
-                    height="12"
-                    width={"40"}
-                  >
-                    Button
-                  </Button>
-                </NextLink>
-              </Box>
-            </WrapItem>
-            <WrapItem>
-              <Box>
-                <Image src="/PlaceholderImageArticle.svg" />
-              </Box>
-            </WrapItem>
-          </Wrap>
-        </Box>
+                    {art.title}
+                  </Text>
+                  <Text color={"neutral.500"} fontWeight={400} mb={[2, 5, 5]}>
+                    {art.description}
+                  </Text>
+                  <NextLink href={"/landingpage"}>
+                    <Button
+                      variant="outline"
+                      borderRadius={5}
+                      borderColor={"neutral.500"}
+                      color={"neutral.500"}
+                      _hover={{}}
+                      height="12"
+                      width={"40"}
+                    >
+                      Button
+                    </Button>
+                  </NextLink>
+                </Box>
+              </WrapItem>
+              <WrapItem>
+                <Box>
+                  <Image src={art.image} />
+                </Box>
+              </WrapItem>
+            </Wrap>
+          </Box>
+        ))}
       </Box>
     </Flex>
   );
--- webapp-next/components/landinglayout/BlockImageGrids.tsx
+++ webapp-next/components/landinglayout/BlockImageGrids.tsx
@@ -9,7 +9,24 @@ import {
   WrapItem,
 } from "@chakra-ui/react";
 
-export default function BlockImageGrids() {
+type ContentsGrids = {
+  title: string;
+  description: string;
+};
+
+type ContentsImage = {
+  image: string;
+};
+
+interface Contentsofpage {
+  contentimage: ContentsImage;
+  contentsgrids: ContentsGrids[];
+}
+
+export const BlockImageGrids: React.FC<Contentsofpage> = ({
+  contentimage,
+  contentsgrids,
+}) => {
   return (
     <Flex flexDir="column" mb={16} pt={[5, 10, 14]} px={[5, 20, 36]}>
       <Box mb={[2, 4, 2]}>
@@ -29,47 +46,31 @@ export default function BlockImageGrids() {
 
       <Box alignItems={"left"}>
         <Image
-          src="/Placeholder-image.svg"
-          alt="Image 1"
+          src={contentimage.image}
+          alt="image"
           height={["sm", "md", "xl"]}
           width={"100%"}
           my={[2, 5, 6]}
         />
       </Box>
       <Wrap justify="left" w={"full"}>
-        <WrapItem flex={2}>
-          <Box>
-            <Text fontSize={["14px", "18px", "24px"]} mb={4}>
-              Explorez les données sur les causes de décès
-            </Text>
-            <Text
-              fontSize={["14px", "16px", "18px"]}
-              fontWeight={400}
-              color={"neutral.500"}
-            >
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-              auctor, est vel ullamcorper accumsan, est mauris fringilla nisi, a
-              egestas tellus nisl ac justo.
-            </Text>
-          </Box>
-        </WrapItem>
-        <WrapItem flex={2}>
-          <Box>
-            <Text fontSize={["14px", "18px", "24px"]} mb={4}>
-              Explorez les données sur les causes de décès
-            </Text>
-            <Text
-              fontSize={["14px", "16px", "18px"]}
-              fontWeight={400}
-              color={"neutral.500"}
-            >
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-              auctor, est vel ullamcorper accumsan, est mauris fringilla nisi, a
-              egestas tellus nisl ac justo.
-            </Text>
-          </Box>
-        </WrapItem>
+        {contentsgrids.map((content, index) => (
+          <WrapItem flex={2}>
+            <Box>
+              <Text fontSize={["14px", "18px", "24px"]} mb={4}>
+                {content.title}
+              </Text>
+              <Text
+                fontSize={["14px", "16px", "18px"]}
+                fontWeight={400}
+                color={"neutral.500"}
+              >
+                {content.description}
+              </Text>
+            </Box>
+          </WrapItem>
+        ))}
       </Wrap>
     </Flex>
   );
-}
+};
