6babf218b91ebe3289997fab45e29c02c55489b4
Clément <clement@numericite.eu>
2023-05-29 14:45:28 +0200

    feat: legend display

--- webapp-next/components/charts/map/Legends.tsx
+++ webapp-next/components/charts/map/Legends.tsx
@@ -1,3 +1,47 @@
+import { Box, Flex, Text } from '@chakra-ui/react';
+
 export function Legends() {
-  return <></>;
+  const legends = [
+    {
+      color: 'green.100',
+      borderColor: 'green.300',
+      content: '0-10%'
+    },
+    {
+      color: 'blue.100',
+      borderColor: 'blue.300',
+      content: '10-20%'
+    },
+    {
+      color: 'orange.100',
+      borderColor: 'orange.300',
+      content: '20-30%'
+    },
+    {
+      color: 'red.100',
+      borderColor: 'red.300',
+      content: '40%+'
+    }
+  ];
+
+  return (
+    <Flex justifyContent="right">
+      {legends.map((legend, index) => (
+        <Flex key={index} alignItems={'center'} mr={4}>
+          <Box
+            w={4}
+            h={4}
+            mr={2}
+            borderRadius="full"
+            borderWidth={1}
+            borderColor={legend.borderColor}
+            bg={legend.color}
+          />
+          <Text as="span" fontSize="lg">
+            {legend.content}
+          </Text>
+        </Flex>
+      ))}
+    </Flex>
+  );
 }
